ef2e916a5eb3f99e76f26acdb9cadb8dcb5ce0eb
Rayr <25773909+Rayrsn@users.noreply.github.com>
2023-08-19 13:29:25 +0300

    Added guess-who

---
+++ domains/guess-who.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+    description: 'Personal website', 
+    domain: 'is-an.app', 
+    subdomain: 'guess-who', 
+    owner: {
+      email: 'rayrsn@proton.me',
+    },
+    record: {
+        NS: ['ns1.epizy.com', 'ns2.epizy.com'],
+    },
+  })
